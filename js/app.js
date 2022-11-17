71ede99b67eef8172c3263f002865ed4520514fa
ahmed-samir-azzam <a.azzaam@2p.com.sa>
2022-11-17 21:57:56 +0200

    Layout

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ const toggelThemeBtn = document.getElementById("toggel-theme-btn");
 const html = document.querySelector("html");
 
 toggelThemeBtn.addEventListener("click", () => {
-    debugger;
     html.classList.toggle("light");
     html.classList.toggle("dark");
 });
